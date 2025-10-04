cb163bf25eb6e5833af68ef46977c621ac07c54b
ismael <ismaelnunes1984@gmail.com>
2025-10-04 14:16:44 -0300

    feat(frontend): make Redesign the default entry (replace classic UI)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Redesign from './pages/Redesign'
 import './styles/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Redesign />
   </React.StrictMode>
 )
-
